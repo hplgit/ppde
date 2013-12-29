df4eca55131042f632292c42bf2f1de4052fb159
Hans Petter Langtangen <hpl@simula.no>
2013-12-29 09:07:25 +0100

    updates

--- INF5631/final_2D_BE_PETSc.py
+++ INF5631/final_2D_BE_PETSc.py
@@ -18,7 +18,7 @@ mpirun -np <number of processes> python backward_diffusion_2d.py
 This can be run in serial with -np 1 or just run with python, ignoring
 mpirun. Note that running this in parallel requires additional
 packages in PETSc.
- 
+
 This scheme is unconditionally stable.
 
 """
@@ -31,21 +31,27 @@ from petsc4py import PETSc
 
 
 def solver_2D_BE_PETSc(I, a, Lx, Ly, Nx, Ny, Cx, Cy, T):
-	
+
 	x = np.linspace(0, Lx, Nx+1); y = np.linspace(0, Ly, Ny+1);
 	dx = x[1] - x[0]; dy = y[1] - y[0];
 	dt = Cx*dx**2/a;   # Based on Cx only
 	Nt = int(round(T/float(dt)))
-	
+
 	t = np.linspace(0, T, Nt+1)
 	t0 = time.clock()
-	
+
+	# OOOOPS! Below Nx*Ny is used, as opposed to (Nx+1)*(Ny+1)!!
+	# What to do with Dirichlet values? In matrix or not?
+	# Think about Neumann conditions and ghost values that are
+	# normally required in parallel computations. Look
+	# at 2D C examples in PETSc doc.
+
 	# We need to set up our big matrix, a five-banded sparse MPI mat.
 	A = PETSc.Mat().createAIJ([Nx*Ny, Nx*Ny],nnz=5)
 	[Istart, Iend] = A.getOwnershipRange()
 	for II in range(Istart, Iend):
 		i=II/Nx; j=II-i*Nx;
-		
+
 		if (i>0):
 			J = II-Nx
 			A.setValue(II,J,-Cx)
@@ -59,28 +65,33 @@ def solver_2D_BE_PETSc(I, a, Lx, Ly, Nx, Ny, Cx, Cy, T):
 			J = II+1
 			A.setValue(II,J,-Cy)
 		A.setValue(II,II,1+2*Cx+2*Cy)
-	
+
 	# And then we get our left-handed and right-handed arrays
 	u, u_1 = A.getVecs()
-	
+
 	"""
 	Important that I(x,y) follows the rule of the matrix system. One example is:
-	
+
 	for i in range(Nx):
 		for j in range(Ny):
 			I[i*Ny+j] = math.exp(-((i*dx-0.5)**2 + (j*dy-0.5)**2)*100)
 	"""
 	u_1.setValues(range(Istart,Iend),I[Istart:Iend])
 	#u_1.setArray(I)   # Initialize RHS
-	
-	# Assemble the arrays and matrix. This distribute all the data.
+
+	# Assemble the arrays and matrix. This distributes all the data.
 	A.assemble(); u.assemble(); u_1.assemble()
-	
+
 	# If not in parallel, one can write to file for plotting in another
 	# program for validation. This is started from the command line using
 	# -toFile
+	# Explanation: must have the whole data set on one processor
+	# for (standard) file writing to be meaningful.
 	if PETSc.Options().getBool('toFile',default=False):
 		if PETSc.COMM_WORLD.getSize() > 1:
+			# More than one process, so we run in parallel.
+			# Let the master process (no 0) turn off the toFile
+			# option.
 			if PETSc.COMM_WORLD.getRank() == 0:
 				print 'Warning: not writing to file (using parallel)'
 				PETSc.Options().setValue('toFile',False)
@@ -88,7 +99,7 @@ def solver_2D_BE_PETSc(I, a, Lx, Ly, Nx, Ny, Cx, Cy, T):
 			print 'Writing to file'
 			W = PETSc.Viewer().createASCII('test2d_7.txt',format=0)
 			u_1.view(W)
-	
+
 	# Set up the solver. See the 1D-implementation for a description of this.
 	ksp = PETSc.KSP()
 	ksp.create()
@@ -96,19 +107,19 @@ def solver_2D_BE_PETSc(I, a, Lx, Ly, Nx, Ny, Cx, Cy, T):
 	ksp.getPC().setType('none')
 	ksp.setFromOptions()  # We use command-line, but set cg (no precon) as default
 	ksp.setOperators(A)
-	
+
 	for t in range(Nt):
 		# Solve for next step using the solver set up above using
 		# command-line options.
 		ksp.solve(u_1,u)
-		
-		# Swap
+
+		# Swap (is this safe, check carefully in serial programs - wave1D)
 		u, u_1 = u_1, u
-		
+
 		# Update if writing to file.
 		if PETSc.Options().getBool('toFile',default=False):
 			u_1.view(W)
-	
+
 	return u_1, time.clock()-t0
 
 
--- INF5631/integdiff.py
+++ INF5631/integdiff.py
@@ -10,26 +10,33 @@ def trapezoidal_PETSc(f, a, b, n):
     using the Trapezoidal rule. PETSc version, which one could
 	be used in parallel.
     """
+	# Let x be distributed too!
+	# This is fine as a 1D integral example, but should use
+	# DA for multiple dimensions.
 	x = np.linspace(a, b, n+1)
 	h = (b-a)/float(n)
 	t0 = time.clock()
+	# Keep f as function, f(x[Istart:Iend])
 	f_vec = f#(x)
 	f_vec[0] /= 2.0
 	f_vec[-1] /= 2.0
-	
+
 	# Create a distributed array, assemble (doing the
 	# distribution), get the local ownership span, and then fill
 	# the ranks localpart of it.
 	Pf = PETSc.Vec().createMPI(f_vec.size,comm=PETSc.COMM_WORLD)
 	Pf.assemble()
 	[Istart, Iend] = Pf.getOwnershipRange()
+	# Now we need to make a local numpy array of length Istart-Iend+1
+	# and fill it, then insert with Pf.setValues
 	Pf.setValues(range(Istart,Iend),f_vec[Istart:Iend])
-	
+	# Modify /= 2 if Istart == 0 or Iend == f_vec.size()-1
+
 	# Now we find the total sum (which apperently also
 	# get scattered to all ranks). Note that in the start
 	# if this routine we divided the end points by 2.
 	tot_sum = h*Pf.sum()
-	
+
 	# Every rank has now a correct sum, and each ranks
 	# version can be use. This is unlike the
 	# differentiate function, where only root rank is correct.
@@ -43,12 +50,17 @@ def differentiate_PETSc(f, a, b, n):
 	a centered difference, while end points apply a one-sided
 	difference. PETSc serial/parallel version.
 	"""
-	
+	# Note: should use a DA in 1D and 2D for holding f values,
+	# call it u and have f(x,y)
+	# Each processor performs its operation in calculating
+	# the gradient, using ghost points from neighbors. Most
+	# of the explicit FD schemes will with this example
+	# be easy to understand.
 	x = np.linspace(a, b, n+1)  # mesh
 	f_vec = f#(x)
 	dx = x[1] - x[0]
 	t0 = time.clock()
-	
+
 	# Only root rank need to allocate a large array for storage.
 	# The otherranks need still to return something, so we set
 	# df to a zero.
@@ -56,13 +68,13 @@ def differentiate_PETSc(f, a, b, n):
 		df = np.zeros_like(x)
 	else:
 		df = np.zeros(1)
-	
+
 	# We need one array containing the left part of the vector,
 	# and one for the right part. This is distributed during the
 	# assembly.
 	diffL = PETSc.Vec().createMPI(f_vec.size-2)
 	diffR = PETSc.Vec().createMPI(f_vec.size-2)
-	
+
 	# Each rank owns its local part, given by getOwnershipRange.
 	# From this we copy each part of f_vec, and then assemble.
 	# As diffL and diffR is the same, their ownership ranges must
@@ -71,30 +83,30 @@ def differentiate_PETSc(f, a, b, n):
 	diffL.setValues(range(Istart,Iend),f_vec[Istart:Iend])
 	diffR.setValues(range(Istart,Iend),f_vec[Istart+2:Iend+2])
 	diffL.assemble(); diffR.assemble()
-	
+
 	# Now we do the calculations. In pure vector numpy code, this
 	# would be:    df[1:-1] = (f_vec[2:] - f_vec[:-2])/(2*dx)
 	diffR.axpby(-1,1,diffL)
 	diffR.scale(1/(2*dx))
-	
+
 	# Now that each rank has calculcated each local part of the
 	# differentiated vector, we need to gather it all to root rank
 	# with the PETSc Scatter module.
 	scatter, diff_ = PETSc.Scatter.toZero(diffR)
 	scatter.scatter(diffR,diff_,False,PETSc.Scatter.Mode.FORWARD)
-	
+
 	# Only root can collect the PETSc array to the Numpy array df.
 	if PETSc.COMM_WORLD.getRank() == 0:
 		df[1:-1] = diff_.getArray()
-		
+
 		# End points
 		df[0]  = (f_vec[1]  - f_vec[0]) /dx
 		df[-1] = (f_vec[-1] - f_vec[-2])/dx
-	
+
 	# Note that all other ranks then rank=0 returns a 0,
 	# that is, pure junk. Timing is returned for benchmarking.
 	return df, time.clock()-t0
-	
+
 
 def differentiate_vec(f, a, b, n):
 	"""
@@ -108,12 +120,12 @@ def differentiate_vec(f, a, b, n):
 	f_vec = f#(x)
 	dx = x[1] - x[0]
 	t0 = time.clock()
-	
+
 	# Internal mesh points
 	df[1:-1] = (f_vec[2:] - f_vec[:-2])/(2*dx)
 	# End points
 	df[0]  = (f_vec[1]  - f_vec[0]) /dx
 	df[-1] = (f_vec[-1] - f_vec[-2])/dx
-	
+
 	return df, time.clock()-t0
 
